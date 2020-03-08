3c8995c29073ec80349033637fb57278e99b2eeb
tomas.hampl <tomas.hampl@zajezdy.cz>
2020-03-08 17:00:54 +0100

    project init

---
+++ src/main/java/biz/hampl/timone/taks1/Taks1Application.java
@@ -0,0 +1,13 @@
+package biz.hampl.timone.taks1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taks1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taks1Application.class, args);
+	}
+
+}
---
+++ src/test/java/biz/hampl/timone/taks1/Taks1ApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.hampl.timone.taks1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taks1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
